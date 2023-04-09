07f4e00cd3c6ae4970d7473cdee25884c445b6d3
peps212 <giuseppe.rapisarda12@gmail.com>
2023-04-09 18:03:43 +0200

    Switched to a conversational model with memory

--- pages/api/agentServerSide.js
+++ pages/api/agentServerSide.js
@@ -7,6 +7,7 @@ import {
   SystemMessagePromptTemplate,
   HumanMessagePromptTemplate,
 } from "langchain/prompts";
+import { BufferMemory } from "langchain/memory";
 
 const KEY = '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';
 const serpKEY = 'cba55f8c3f206322ffdd3a924d1ce2215a5c4bb382b3783dc52d306bcc0c20e7';
@@ -16,15 +17,23 @@ export default async function handler(req, res) {
   try {
     const model = new ChatOpenAI({ openAIApiKey: KEY, temperature: 0 });
     const tools = [new SerpAPI(serpKEY)];
+    const serpapi = tools[0]
+    serpapi.description = "This tool allows you to search information you dont know on the internet. Only use it when someone asks you about specific information about the airline company Etihad"
+    console.log(serpapi.description)
+
 
     const executor = await initializeAgentExecutor(
       tools,
       model,
-      "zero-shot-react-description",
+      "chat-conversational-react-description",
       true
     );
+    executor.memory = new BufferMemory({
+      returnMessages: true,
+      memoryKey: "chat_history",
+      inputKey: "input",
+    });
 
-    
     const input = req.body.input;
     
     const result = await executor.call({ input });
